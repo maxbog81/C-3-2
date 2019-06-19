3717638b19d577fca1d936d55ef695c5aaa49f6c
Maksim Bogatov <maxbog@inbox.ru>
2019-06-19 23:31:23 +0300

    Урок 2

---
+++ MailSender06_19-master/MailSender/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+using MailSender.Components;
+
+namespace MailSender
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void OnExitClick(object Sender, RoutedEventArgs E) => Close();
+
+        private void TabController_OnLeftButtonClick(object Sender, EventArgs e)
+        {
+            if(!(Sender is TabController tab_controller)) return;
+
+            if(tab_controller.IsLeftButtonVisible)
+                MainTabControl.SelectedIndex--;
+
+            tab_controller.IsLeftButtonVisible = MainTabControl.SelectedIndex > 0;
+            tab_controller.IsRightButtonVisible = MainTabControl.SelectedIndex < MainTabControl.Items.Count;
+        }
+
+        private void TabController_OnRightButtonClick(object Sender, EventArgs e)
+        {
+            if(!(Sender is TabController tab_controller)) return;
+
+            if(tab_controller.IsRightButtonVisible)
+                MainTabControl.SelectedIndex++;
+
+            tab_controller.IsLeftButtonVisible = MainTabControl.SelectedIndex > 0;
+            tab_controller.IsRightButtonVisible = MainTabControl.SelectedIndex < MainTabControl.Items.Count - 1;
+        }
+
+
+        private void BtnClock_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabControl.SelectedItem = TabPlanner;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BodyText.Text))
+            {
+                MessageBox.Show("Письмо не заполнено");
+                MainTabControl.SelectedIndex = 2;
+            }
+        }
+    }
+}
